3d6ece31ef6b286649044b78e224b14c9305fffe
jacoy96 <34117259+jacoy96@users.noreply.github.com>
2019-04-18 12:09:41 -0400

    Admin

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ let bueinesses = [
   {
     id: 34378,
     name: "B&B Company",
-    status: business_status_init
+    status: business_status_approved
   },
   {
     id: 34379,
@@ -25,13 +25,65 @@ let bueinesses = [
   {
     id: 34372,
     name: "VTDI Company",
-    status: business_status_init
+    status: business_status_approved
   },
 
   {
     id: 34371,
     name: "UTECH Company",
     status: business_status_init
+  }
+];
+
+let peending = [
+{
+  id: 85521,
+  name: "Jamdung City",
+  status: business_status_pending
+},
+{
+  id: 85522,
+  name: "Little Pub",
+  status: business_status_pending
+},
+{
+  id: 85523,
+  name: "Elephant Town",
+  status: business_status_pending
+},
+{
+  id: 85524,
+  name: "Great Pond Primary",
+  status: business_status_pending
+},
+];
+
+let bloocked = [
+  {
+    id: 48840,
+    name: "Gyal Town Club",
+    status: business_status_blacklisted
+  },
+  {
+    id: 48841,
+    name: "B&B UpTown",
+    status: business_status_blacklisted
+  },
+  {
+    id: 48842,
+    name: "ABS Police Station",
+    status: business_status_blacklisted
+  },
+  {
+    id: 48843,
+    name: "Get Rekt Gaming Lounge",
+    status: business_status_blacklisted
+  },
+
+  {
+    id: 48844,
+    name: "Uwi Mona",
+    status: business_status_blacklisted
   }
 ];
 
@@ -66,4 +118,68 @@ ons.ready(function() {
     }
     console.log(e);
   });
+
+  document.addEventListener("init", function(e) {
+    if (e.target.id === "businessPage") {
+      const lstPending = document.querySelector("#lstPending");
+
+      lstPending.innerHTML = "";
+
+      for (let i = 0; i < peending.length; i++) {
+        const item = document.createElement("ons-list-item");
+        item.innerHTML =
+         peending[i].name  + peending[i].status.toString();
+        lstPending.appendChild(item);
+      }
+    }
+    console.log(e);
+  });
+
+  document.addEventListener("init", function(e) {
+    if (e.target.id === "businessPage") {
+      const lstBlocked = document.querySelector("#lstBlocked");
+
+      lstBlocked.innerHTML = "";
+
+      for (let i = 0; i < bloocked.length; i++) {
+        const item = document.createElement("ons-list-item");
+        item.innerHTML =
+         bloocked[i].name  + bloocked[i].status.toString();
+        lstBlocked.appendChild(item);
+      }
+    }
+    console.log(e);
+  });
+
 });
+
+function changeTab() {
+  document.getElementById('tabbar').setActiveTab(1);
+}
+
+document.getElementById('tabbar__button').addEventListener('click', function () {
+  toggle(document.querySelectorAll('.target'));
+});
+
+function toggle (elements, specifiedDisplay) {
+var element, index;
+
+elements = elements.length ? elements : [elements];
+for (index = 0; index < elements.length; index++) {
+  element = elements[index];
+
+  if (isElementHidden(element)) {
+    element.style.display = '';
+
+    // If the element is still hidden after removing the inline display
+    if (isElementHidden(element)) {
+      element.style.display = specifiedDisplay || 'block';
+    }
+  } else {
+    element.style.display = 'none';
+  }
+}
+function isElementHidden (element) {
+  return window.getComputedStyle(element, null).getPropertyValue('display') === 'none';
+}
+}
